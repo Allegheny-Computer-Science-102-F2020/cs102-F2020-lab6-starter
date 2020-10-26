2bdd4194eefee894ae3502fdfbfcbceaf4d7c6ce
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2020-10-26 14:43:51 -0400

    Add the test_fibonacci.py with the correct TODO markers.

---
+++ fibonaccicreator/tests/test_fibonacci.py
@@ -0,0 +1,14 @@
+"""Add test cases for the functions in the fibonacci module."""
+
+import sympy
+
+
+from fibonaccicreator import fibonacci
+
+
+# TODO: Add the test cases from the practical assignment
+#
+# TODO: At your option, add more test cases for the new function
+#
+# TODO: Confirm that all of the test cases are passing correctly
+# before you work on the experiments for this assignment!
